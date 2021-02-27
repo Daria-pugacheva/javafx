6ceca557b346fa5f7175765717dd4d4529f02d52
geekbrainsPugachevaD <dpugachev@uandex.ru>
2021-02-28 00:50:11 +0300

    ДЗ по 4-му уроку. Окно в JavaFX клиентской части чата

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+
+    private String userName = "Daria";
+
+    @FXML
+    TextArea chat;
+
+    @FXML
+    TextField message;
+
+    public void sendMsg() {
+        if (message.getLength() > 0) {
+            chat.appendText(userName + ":\n" + message.getText() + "\n\n");
+            message.clear();
+        }
+    }
 }
